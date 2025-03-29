dcf136c84678a6e54c3b70371f94a95aa2ef9148
Adrian Tarantino <adriantarantino2006@gmail.com>
2025-03-28 21:01:15 -0400

    Adding Ingredients to "More Information" tab

--- app/src/app/view-recipes/page.tsx
+++ app/src/app/view-recipes/page.tsx
@@ -23,6 +23,11 @@ const ViewRecipes = () => {
       }))
     })
   })
+  
+  const getIngredientQuantity = (ingredientName: string): string => {
+    if (typeof(ingredientName.split(" [")[1]) != "undefined") return ingredientName.split(" [")[1].split("]")[0]
+    return ""
+  }
 
   return (
     <div className="pt-3 w-dvw flex flex-col justify-center items-center">
@@ -92,7 +97,7 @@ const ViewRecipes = () => {
               </div>
             </div>
             <dialog id={recipe.Title+'-modal'} className="modal">
-              <div className="modal-box">
+              <div className="modal-box h-96 no-scrollbar">
                 {/* <figure className="px-10 pt-10">
                   <img
                     src={recipe.id}
@@ -100,10 +105,26 @@ const ViewRecipes = () => {
                     className="rounded-xl" />
                 </figure> */}
                 <h3 className="font-bold text-lg">{recipe.Title}</h3>
-                <p className="py-4 text-lg">{recipe.Description}</p>
+                <div className="overflow-y-scroll no-scrollbar max-h-60">
+                  <p className="font-bold py-4 text-lg">Description</p>
+                  <p className="py-4 text-lg">{recipe.Description}</p>
+
+                  <p className="font-bold py-4 text-lg">Ingredients</p>
+
+                  <table className="table table-zebra">
+                    <tbody>
+                      {recipe.Ingredients.map((ingredient) => (
+                        <tr key={ingredient}>
+                          <th>{ingredient.split(" [")[0]}</th>
+                          <td>{getIngredientQuantity(ingredient)}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
                 <div className="modal-action">
                   <button className="btn"><svg className="fill-base-content" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.753 15.467c1.301 1.821.939 3.104 2.247 4.938l-5.041 3.595c-2.723-2.027-5.072-2.677-5.83-2.932-.723-.243-1.189-.706-1.029-1.289.164-.589.779-.764 1.286-.741.765.035 1.386.265 1.386.265l-3.516-4.93c-.314-.44-.211-1.051.229-1.365s1.05-.211 1.363.229l2.383 3.333c.114.161.338.199.498.084.162-.115.199-.339.085-.5l-.587-.823.944-.235c.248-.06.507.036.655.244l.48.673c.115.161.338.199.499.084s.197-.338.083-.5l-.555-.777.928-.208c.243-.052.495.045.64.247l.407.572c.114.161.339.198.5.084.16-.115.198-.339.084-.5l-.458-.641.273-.048c.952-.167 1.468.329 2.046 1.141zm-10.838-3.248c.61-.436 1.399-.45 1.987.002-.335-1.121-1.676-1.583-2.63-.902-.955.681-.952 2.099-.002 2.779-.235-.703.035-1.444.645-1.879zm1.577 10.745c-.682-.229-1.188-.571-1.569-.964h-6.923v-14h3v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h6v1.5c0 .276.224.5.5.5s.5-.224.5-.5v-1.5h3v4.6c.541-.098 1.486-.294 2-.302v-6.298h-5v-2c0-2.209-1.791-4-4-4s-4 1.791-4 4v2h-5v18h13.134c-1-.49-1.683-.723-2.642-1.036zm-4.492-18.964c0-1.654 1.346-3 3-3s3 1.346 3 3v2h-6v-2z"/></svg></button>
-                  <button className="btn" onClick={() => { open(recipe.id) }}>Source</button>
+                  <button className="btn" onClick={() => { open(recipe.Source) }}>Source</button>
                   <button className="btn" onClick={() => { document.getElementById(recipe.Title+'-modal')!.classList.remove('modal-open'); }}>Close</button>
                 </div>
               </div>
